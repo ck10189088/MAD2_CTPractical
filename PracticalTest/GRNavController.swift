0881cd3db5b9bc1983a392de98f8f42c0b10982a
farm <s10189906@connect.np.edu.sg>
2019-12-05 13:53:27 +0800

    4 dec

--- PracticalTest/GRNavController.swift
+++ PracticalTest/GRNavController.swift
@@ -10,9 +10,24 @@ import Foundation
 import UIKit
 
 class GRNavController : UITableViewController{
+    let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        appDelegate.createDummyContacts()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        appDelegate.contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath){
+            
+        }
+    }
 }
