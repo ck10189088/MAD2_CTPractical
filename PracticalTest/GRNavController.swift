b9c36d5015c04c4b14adc22fd9b2e119e334accd
farm <s10189906@connect.np.edu.sg>
2019-12-05 11:34:41 +0800

    5 December

---
+++ PracticalTest/GRNavController.swift
@@ -0,0 +1,14 @@
+//
+//  GRNavController.swift
+//  PracticalTest
+//
+//  Created by MAD2_P02 on 5/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GRNavController : UIViewController{
+    
+}
--- PracticalTest/ViewController.swift
+++ PracticalTest/ViewController.swift
@@ -19,12 +19,24 @@ class ViewController: UIViewController {
         var accountlist:[NSManagedObject] = []
         
         let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         appDelegate.StoreTestAccount()
         
-        let fetchRequest = NSFetchRequest<NSManagedObject>
-        
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "")
-        present(vc!,animated: false,completion: nil)
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "CDAccount")
+        do{
+            accountlist = try context.fetch(fetchRequest)
+            
+            for a in accountlist{
+                let email = a.value(forKeyPath: "email") as? String
+                let password = a.value(forKeyPath: "password") as? String
+                if(txtEmail.text == email && txtPassword.text == password){
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "grnavcontroller")
+                    present(vc!,animated: false,completion: nil)
+                }
+            }
+        }catch let error as NSError{
+            print("Wrong email or password. \(error),\(error.userInfo)")
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
